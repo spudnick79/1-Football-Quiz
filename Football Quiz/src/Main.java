610b35a742418f45c6c038801d21291f312daf2e
ntgadomski3779 <ntgadomski3779@7127R1S2.STUDENT.ad.fgcu.edu>
2018-09-27 10:26:03 -0400

    Initial commit

---
+++ Football Quiz/src/Main.java
@@ -0,0 +1,10 @@
+// Nickolas Gadomski
+// A Football Quiz with 2 modes Easy-simple and Hard-statistic Quiz
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Football Quiz Game!");
+
+	}
+
+}
