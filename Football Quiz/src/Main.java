9b48010f64c98a4f8de99a845cb7a2bf21a238f1
ntgadomski3779 <ntgadomski3779@7127R1S2.STUDENT.ad.fgcu.edu>
2018-10-04 09:57:57 -0400

    Added Switch for Mode

--- Football Quiz/src/Main.java
+++ Football Quiz/src/Main.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 // Nickolas Gadomski
 // A Football Quiz with 2 modes Easy-simple and Hard-statistic Quiz
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Football Quiz Game!");
-
+		System.out.println("Choose your mode, either Easy or Hard");
+		
+		int selection;
+		Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter a menu selection");
+	    System.out.println("1. Easy");
+	    System.out.println("2. Hard");
+	    selection = scan.nextInt();
+		
 	}
 
 }
