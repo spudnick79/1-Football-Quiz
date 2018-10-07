8cd98e671869c6573f47e3de99835b2209a4cc94
Nickolas Gadomski <spudnick79@gmail.com>
2018-10-07 18:33:01 -0400

    Added methods and for loop to easy mode

--- Football Quiz/src/Main.java
+++ Football Quiz/src/Main.java
@@ -14,7 +14,54 @@ public class Main {
 	    System.out.println("1. Easy");
 	    System.out.println("2. Hard");
 	    selection = scan.nextInt();
-		
-	}
+	    
+	    //Questions For Easy
+	    String Q1 = "How many receivers has Tom Brady Thrown a TD too in his career?/n"
+	    		+ "(a)73/n(b)71/n(c)69/n";
+	    String Q2 = "How many superbowls have the Eagles won?/n"
+	    		+ "(a)1/n(b)5/n(c)3";
+	    
+	    Question [] questions = {	//creates an array for questions
+    			new Question(Q1, "b"),
+    			new Question(Q2, "a")
+	    };
+	    takeTest(questions);
+	    
+	    switch (selection){
+	    case 1:
+	    	public static void takeTest(Question[] questions) {	//added method to start easy mode test
+	    		int score = 0;
+	    		Scanner keyboardInput = new Scanner(System.in);
+	    		
+	    		for(int i = 0; i < questions.length; i++){	//created for loop to add score
+	    			System.out.println(questions[i].prompt);
+	    			String answer = keyboardInput.nextLine();
+	    			if(answer.equals(questions[i].answer));
+	    				score++;
+	    		}
+	    		
+	    	System.out.println("You got " + score + "/" + questions.length);
+	    	}
+	    break;
+	    case 2:
+	    	
+	    }
 
+	    	
+	
+	
+	    
+	    
+	}	
 }
+	
+	  
+	
+	    
+	    
+	
+	    
+	    
+	    
+		
+
