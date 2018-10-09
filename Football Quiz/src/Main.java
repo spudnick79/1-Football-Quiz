63deb9b9fd7a51bc8e62cd30cd8ffebcbc2b6008
ntgadomski3779 <ntgadomski3779@7127R4S1.STUDENT.ad.fgcu.edu>
2018-10-09 10:23:01 -0400

    Reworked and added two arrys for both questions and answers

--- Football Quiz/src/Main.java
+++ Football Quiz/src/Main.java
@@ -7,68 +7,49 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Football Quiz Game!");
 		System.out.println("Choose your mode, either Easy or Hard");
-		
+
 		int selection;
 		Scanner scan = new Scanner(System.in);
-	    System.out.println("Enter a menu selection");
-	    System.out.println("1. Easy");
-	    System.out.println("2. Hard");
-	    selection = scan.nextInt();
-	    
+		System.out.println("Enter a menu selection");
+		System.out.println("1. Easy");
+		System.out.println("2. Hard");
+		selection = scan.nextInt();
+		scan.nextLine();
+		
+		switch (selection) {
+		case 1:
 
-	    
-	    switch (selection){
-	    case 1:
+			// Questions For Easy
+			String Q1 = "How many receivers has Tom Brady Thrown a TD too in his career?\n" + "(a)73\n(b)71\n(c)69\n";
+			String Q2 = "How many superbowls have the Eagles won?\n" + "(a)1\n(b)5\n(c)3";
+			
+			String[] answers = new String[2];
+			answers[0] = "b";
+			answers[1] = "a";
 
-		    //Questions For Easy
-		    String Q1 = "How many receivers has Tom Brady Thrown a TD too in his career?/n"
-		    		+ "(a)73/n(b)71/n(c)69/n";
-		    String Q2 = "How many superbowls have the Eagles won?/n"
-		    		+ "(a)1/n(b)5/n(c)3";
-		    
-		    Question [] questions = {	//creates an array for questions
-	    			new Question(Q1, "b"),
-	    			new Question(Q2, "a")
-		    };
-		    takeTest(questions);	//call to array of questions
-		    
-		    public static void takeTest(Question[] questions) {	//added method to start easy mode test
-	    		int score = 0;
-	    		Scanner keyboardInput = new Scanner(System.in);
-	    		
-	    		for(int i = 0; i < questions.length; i++){	//created for loop to add score
-	    			System.out.println(questions[i].prompt);
-	    			String answer = keyboardInput.nextLine();
-	    			if(answer.equals(questions[i].answer));
-	    				score++;
-	    		
-	    		System.out.println("You got " + score + "/" + questions.length);		
-	    		}
-		    }
-		    }					
-	    
-  		
-  	
-	
-	
-	
+			String[] questions = new String[2]; // creates an array for questions
+			questions[0] = Q1;
+			questions[1] = Q2;
 
-	    	
-	
-	
-	    
-	    
-	
+			takeTest(questions, scan); // call to array of questions
+			break;
+		
+		}
+	}
 
-
-	
-	  
-	
-	    
-	    
-	
-	    
-	    
-	    
+	public static void takeTest(String[] answers, Scanner keyboardInput) { // added method to start easy mode test
+		int score = 0;
 		
 
+		for (int i = 0; i < answers.length; i++) { // created for loop to add score
+			System.out.println(answers[i]);
+			String userAnswers = keyboardInput.nextLine();
+			if (userAnswers.equals(answers[i])) {
+
+			score++;
+			}
+			
+		}
+		System.out.println("You got " + score + "/" + answers.length);
+	}
+}
