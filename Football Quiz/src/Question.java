5437d9d1dbe6a77454bf405153080951c1a90666
Nickolas Gadomski <spudnick79@gmail.com>
2018-10-10 21:36:27 -0400

    Added more comments, tried to add Random Class

---
+++ Football Quiz/src/Question.java
@@ -0,0 +1,10 @@
+// class with method to give question initial info
+public class Question {
+	String prompt;
+	String answer;
+	
+	public Question(String prompt, String answer) {
+		this.prompt = prompt;
+		this.answer = answer;
+	}
+}
